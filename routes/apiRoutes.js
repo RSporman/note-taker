8c4f31fe5065f89c4b7a4aea3525a61c88cf4cbf
Bobby Sporman <bobby.sporman@yahoo.com>
2020-08-16 12:16:23 -0400

    updates to DB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,17 +4,20 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var noteData = require("../db/db.json");
+// var noteData = require("../db/db.json");
 
+const fs = require('fs');
+const path = require('path');
 
-
-
-
-// ===============================================================================
-// ROUTING
+// ===============================================================================// ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
+
+  fs.readFile("../db/db.json"),"utf8", function(err, data) {
+    if (err) throw err;
+    var noteData = JSON.parse(data);
+  
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
@@ -26,15 +29,23 @@ module.exports = function(app) {
   });
 
   app.post("/api/notes", function(req, res){
-    console.log(req.body);
-    // ID needs a random number (math.random or uuid package)
-    req.body.id = 2; 
-    console.log(req.body);
-    noteData.push(req.body);
+    let newNote = req.body;
+    noteData.push(newNote);
+    updateDb();
+    return console.log("Added new note: " + newNote.title);
 
-    res.json(noteData);
+    // console.log(req.body);
+    // ID needs a random number (math.random or uuid package)
+    // req.body.id = 2; 
+    // console.log(req.body);
+    // noteData.push(req.body);
+
+    // res.json(noteData);
   })
 
+  app.get("/api/notes/:id", function (req, res){
+    res.json(noteData[req.param.id]);
+  })
 // req.body - access to the new note
 // need to add an ID to the note 
 // put the new note in noteData.push -> req.body
@@ -46,11 +57,17 @@ module.exports = function(app) {
 app.delete("/api/notes/:id", function(req, res){
   //  below to get to the ID and reference it
   req.params.id
+  updateDb()
 })
 
 
 
+function updateDb() {
+  fs.writeFile("../db/db.json", JSON.stringify(noteData,'\t'), function(err){
+      if (err) throw err;
+      return true;
+  });
+}
 
-  
- 
-};
+  }
+}
